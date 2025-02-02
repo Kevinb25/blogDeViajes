504b47718b9192f9f6c6d59f5d022b740af2af94
Kevin Batista <kevinbatistad@gmail.com>
2025-02-02 14:35:51 +0100

    se inicia el proyecto blogDeViajes, se crean dos interfaces y un servicio

---
+++ src/app/interfaces/ipost.interface.ts
@@ -0,0 +1,11 @@
+import { ICategory } from "./icategory.interface";
+
+export interface IPost {
+    id: number,
+    titulo: string,
+    texto: string,
+    autor: string,
+    imagen: string,
+    fecha: string,
+    categoria: ICategory
+}
