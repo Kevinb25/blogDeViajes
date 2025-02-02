0bddca2cc518586c98a6ee9b2d4328ead163bcd9
Kevin Batista <kevinbatistad@gmail.com>
2025-02-02 15:18:28 +0100

    se creo el componente header y 3 paginas (home,new y post)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewComponent } from './pages/new/new.component';
+import { PostComponent } from './pages/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: 'full', redirectTo: 'home' },
+    { path: "home", component: HomeComponent },
+    { path: "new", component: NewComponent },
+    { path: "post", component: PostComponent },
+
+];
